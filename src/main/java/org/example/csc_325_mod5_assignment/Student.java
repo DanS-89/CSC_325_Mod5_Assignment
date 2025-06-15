5f019a911f35f681a8f34880501928fe699214c2
DanS-89 <stevens.j.daniel@gmail.com>
2025-06-15 17:50:31 -0400

    GUI Build - added student class

--- src/main/java/org/example/csc_325_mod5_assignment/Student.java
+++ src/main/java/org/example/csc_325_mod5_assignment/Student.java
@@ -1,5 +1,70 @@
 package org.example.csc_325_mod5_assignment;
 
 public class Student {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String department;
+    private String major;
+    private String email;
+    private String imageUrl;
 
+    public Student(int id, String firstName, String lastName, String department, String major, String email, String imageUrl) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.department = department;
+        this.major = major;
+        this.email = email;
+        this.imageUrl = imageUrl;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
 }
